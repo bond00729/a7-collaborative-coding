3a5ec6c13d68d9192bb25ee7e2903d9db832d4e3
Brandon <brandon42496@yahoo.com>
2016-02-24 12:07:22 -0800

    passed in the data as a parameter

--- scripts/datachart1.R
+++ scripts/datachart1.R
@@ -1,10 +1,9 @@
 #Script for the data visualization
 
-build_chart <- function(){
+build_chart <- function(data){
   library(plotly)
   library(dplyr)
   
-  data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
   formatted_data <- select(data, What.is.your.current.class.standing., What.is.your.programming.experience.)
   count_data <- group_by(formatted_data, What.is.your.programming.experience., 
                          What.is.your.current.class.standing.) %>% 
